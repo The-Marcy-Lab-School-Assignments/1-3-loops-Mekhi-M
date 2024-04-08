b1ef7e5015c507832c9d781ed4fc880f9cf6bced
Mekhi-M <mekhim0731@gmail.com>
2024-04-08 08:34:12 -0400

    debug (9-10) done

--- src/debug.js
+++ src/debug.js
@@ -1,20 +1,29 @@
 const brokenLoop = (start, end) => {
   let result = '';
-  for (let i = start; i < end; i += 2) {
+  for (let i = start; i <= end; i ++) {
     result += i;
   }
   return result;
 };
 
+// tests
+
+// console.log(brokenLoop(1, 10))
+// console.log(brokenLoop(5, 12))
+// console.log(brokenLoop(0, 0))
+// console.log(brokenLoop(0, -1))
+
 const brokenNested = () => {
   let result = '';
   for (let i = 0; i < 2; i++) {
-    for (let i = 0; i < 10; i++) {
-      result += `-${i}${i}`;
+    for (let j = 0; j < 10; j++) {
+      result += `-${i}${j}`;
     }
   }
   return result.slice(1);
 };
+
+console.log(brokenNested())
 
 module.exports = {
   brokenLoop,
