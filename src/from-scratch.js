1c70e01ac4a35668c8c53270b0a65116c5c93727
Mekhi-M <mekhim0731@gmail.com>
2024-04-08 08:41:27 -0400

    fromScratch (10) done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -52,7 +52,20 @@ const loopUpToNum = (num) => {
 // loopUpToNum(-7)
 
 const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log('buzz');
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else {
+      console.log(i);
+    }
+  }
 };
+
+fizzbuzz();
 
 module.exports = {
   loop0UpTo10,
